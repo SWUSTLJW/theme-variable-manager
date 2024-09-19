75cc11a6468ccb626dcde7995264c9ea780a4362
liaojiawei <Swustljw@lednets.com>
2024-09-19 14:29:40 +0800

    源码打包混淆webpack配置

---
+++ webpack.backend.config.js
@@ -0,0 +1,69 @@
+// webpack.server.config.js
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+const webpack = require("webpack");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./index.js", // 入口文件
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.bundle.js",
+  },
+  target: "node",
+  mode: "production",
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: true,
+          mangle: true,
+        },
+      }),
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: "#!/usr/bin/env node",
+      raw: true,
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "package.json",
+          to: "package.json",
+          transform(content) {
+            // 修改 package.json 内容
+            const packageJson = JSON.parse(content.toString());
+
+            // 在这里对 package.json 进行修改
+            // 例如，将 bin 和 main 字段指向 dist 目录
+            packageJson.main = "index.bundle.js";
+            packageJson.bin = {
+              "theme-variable-manager": "./index.bundle.js",
+            };
+
+            // 将修改后的内容转换为 Buffer
+            return Buffer.from(JSON.stringify(packageJson, null, 2), "utf-8");
+          },
+        }, // 复制 package.json 到 dist 目录
+        { from: "README.md", to: "README.md" }, // 复制 README.md 到 dist 目录
+      ],
+    }),
+  ],
+};
---
+++ webpack.frontend.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/public/index.html', // 前端 JavaScript 入口文件
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js', // 输出的 JavaScript 文件
+    clean: true, // 清理 /dist 文件夹
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // 处理 JavaScript 文件
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.html$/, // 处理 HTML 文件
+        use: 'html-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/public/index.html', // 原始 HTML 文件路径
+      filename: 'index.html', // 输出的 HTML 文件名
+      inject: 'body', // 将脚本注入到 body 中
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: true, // 启用压缩
+          mangle: true,   // 启用混淆
+        },
+      }),
+    ],
+  },
+  stats: {
+    children: true, // 显示子编译错误
+  },
+};
